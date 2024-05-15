919d6ab534de222ce9f53395df08b3f182d6df4e
Zarina Murzabekova <murza46@gmail.com>
2024-05-15 14:08:09 +0400

    Добавлено: отступы

--- test_hw_py2.py
+++ test_hw_py2.py
@@ -6,6 +6,7 @@ def test_search():
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
+
 def test_text_not_found():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('hvbherfbvrrygsvyrslffrgtpoliedrfgthyujikolp45677899').press_enter()
